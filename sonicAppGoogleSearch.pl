7af39b76c770bdc852e248d425727482dd2ce114
Matthew Malone <m.malone@me.com>
2020-01-13 22:12:58 -0700

    Setup MOJO to return Google search results in HTML and parse response down to the main div tags

--- sonicAppGoogleSearch.pl
+++ sonicAppGoogleSearch.pl
@@ -1,21 +1,19 @@
 #!/usr/bin/perl
-# use strict;
-use LWP::UserAgent;
+use Mojo::UserAgent;
+use Mojo::DOM;
 
 print "Enter what you\'d like to search Google for: ";
 $searchQuery=<STDIN>;
-print "Searching google for: $searchQuery";
-$ua = new LWP::UserAgent;
-$ua->timeout(30);
-$ua->agent(" Mozilla/5.0 (0wn3d; U; PPC Sik OS XXX Mach-O; en-US; rv:1.8.1) Gecko/20061010 Firefox/2.0");
-$url = "http://www.google.com/search?q=\"$searchQuery\"&hl=id&lr=&start=0&sa=N";
 
-$response = $ua->get($url);
-if(!($response->is_success)){
-     print ($response->status_line. "     Error! \n");
-    print "Response is not a success"
-} else {
-    @results = $response->as_string;
-    $results= "@results";
-    print "TheSE ARE THE RESuLTS: $results";
-}
+print "Searching google for: $searchQuery"; 
+$ua = new Mojo::UserAgent; 
+$url = "http://www.google.com/search?q=\"$searchQuery\"&hl=id&lr=&start=1&sa=N";
+$response = $ua->get($url)
+    ->res
+    ->dom;
+$dom = Mojo::DOM->new( $response );
+$href_list = $dom
+    ->find('div[id="main"]')    
+    ->join("\n");
+
+print "Google Searh Results in HTML: $href_list";
